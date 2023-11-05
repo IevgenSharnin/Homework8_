715b4d1231ea7f57bfefc0f0d84bd53215b4806f
IevgenSharnin <83iwert@gmail.com>
2023-11-05 15:01:37 +0200

    Final for send

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from datetime import date, datetime, timedelta
 
-#today = (datetime(2021, 12, 27)).date()
-today = date.today()
-if today.weekday() == 0: # Коригуємо поточну дату, щоб забрати суботу-неділю,
-    today = today - datetime.timestamp (days = 2) # якщо сьогодні понеділок
-
 def get_birthdays_per_week(users):
     # Реалізуйте тут домашнє завдання
     # Змінні
     users_next_week = {} # Результат функції
     weekdays_indexes = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday',
                         3: 'Thursday', 4: 'Friday'}
-    today = date.today()
-#    today = (datetime(2021, 12, 27)).date()
-    if today.weekday() == 0: # Коригуємо поточну дату, щоб забрати суботу-неділю,
-        today = today - timedelta (days = 2) # якщо сьогодні понеділок
+    today_ = date.today()
+#    today_ = datetime(2023, 1, 2).date()
+    if today_.weekday() == 0: # Коригуємо поточну дату, щоб забрати суботу-неділю,
+        today_ = today_ - timedelta (days = 2) # якщо сьогодні понеділок
+    if today_.weekday() == 6: # Коригуємо поточну дату, щоб забрати суботу,
+        today_ = today_ - timedelta (days = 1) # якщо сьогодні неділя
 
     # Перевірка на пустий список, що прийшов як аргумент 
     if users == []:
@@ -22,38 +19,55 @@ def get_birthdays_per_week(users):
 
     # Проганяємо users через цикл та заповнюємо словник
     for person in users:
-        print ((today - person.get('birthday')).days)
-        today_wo_year = today - datetime(today.year, 1, 1).date()
+        # Кількість днів від початку поточного року до сьогодні
+        today_wo_year = today_ - datetime(today_.year, 1, 1).date()
+        # Кількість днів від сьогодні до кінця року
+        today_to_endyear = today_ - datetime(today_.year, 12, 31).date()
+        # Кількість днів від початку року народження до дати народження
         user_bday = person.get('birthday') - datetime(person.get('birthday').year, 1, 1).date()
-        delta_to_today = today - datetime(today.year, person.get('birthday').month, 
+        # Дельта від сьогодні до дня народження без урахування року
+        if (0 >= today_to_endyear.days > -7) and (user_bday.days < 7):
+            delta_to_today = today_ - datetime(today_.year+1, person.get('birthday').month, 
                                      person.get('birthday').day).date()
-        print (today_wo_year, user_bday, delta_to_today)
-        if 0 >= delta_to_today.days >= -7:
-            day_of_week_birth_today_year = datetime(today.year, person.get('birthday').month, 
-                                     person.get('birthday').day).weekday
-            print(datetime(today.year, person.get('birthday').month, 
-                                     person.get('birthday').day).weekday)
-            print (person.get('birthday'), day_of_week_birth_today_year)
+        else:
+            delta_to_today = today_ - datetime(today_.year, person.get('birthday').month, 
+                                     person.get('birthday').day).date()
+# Перевіряємо, що дата народження на наст.тижні і формуємо вихідний словник
+        if 0 >= delta_to_today.days > -7:
+            day_of_week_birth_today_year = datetime(today_.year, person.get('birthday').month, 
+                                     person.get('birthday').day).weekday()
             if day_of_week_birth_today_year == 5 or day_of_week_birth_today_year == 6:
                 day_of_week_birth_today_year = 0
             list_for_append = users_next_week.get (weekdays_indexes.get(day_of_week_birth_today_year), [])
             list_for_append.append (person.get('name'))
             users_next_week.update ({weekdays_indexes.get(day_of_week_birth_today_year):
-                                          list_for_append})
+                                    list_for_append})
 
     return users_next_week
 
 
 if __name__ == "__main__":
+    today_ = date.today()
+    #today_ = datetime(2023, 1, 2).date()
+    if today_.weekday() == 0: # Коригуємо поточну дату, щоб забрати суботу-неділю,
+        today_ = today_ - timedelta (days = 2) # якщо сьогодні понеділок
+    if today_.weekday() == 6: # Коригуємо поточну дату, щоб забрати суботу-неділю,
+        today_ = today_ - timedelta (days = 1) # якщо сьогодні понеділок
+
     users = [
-            {
-                "name": "Alice",
-                "birthday": (datetime(2019, 11, 6)).date(),
-            },
+#            {
+#                "name": "John",
+#                "birthday": (today_ + timedelta(days=-5)),
+#            },
+#            {
+#                "name": "Doe",
+#                "birthday": (today_ + timedelta(days=-6)),
+#            },
+            {"name": "Alice", "birthday": (today_ + timedelta(days=3))},
     ]
 
     result = get_birthdays_per_week(users)
     print(result)
     # Виводимо результат
-    for day_name, names in result.items():
-        print(f"{day_name}: {', '.join(names)}")
+#    for day_name, names in result.items():
+#        print(f"{day_name}: {', '.join(names)}")
